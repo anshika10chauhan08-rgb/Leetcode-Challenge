748c8e7ed0572fda9f93e8450af80bb21432895a
Anshika Chauhan <anshika10chauhan08@gmail.com>
2026-09-19 22:28:02 +0530

    Time: 454 ms (50.78%), Space: 79.6 MB (47.39%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -5,27 +5,28 @@ class Solution {
             return 0;
         }
 
-        boolean[] isPrime = new boolean[n];
-
+        byte[] prime = new byte[n];
         int count = 1; // 2 is prime
 
+        // Mark odd numbers as prime
         for (int i = 3; i < n; i += 2) {
-            isPrime[i] = true;
+            prime[i] = 1;
         }
 
+        // Sieve
         for (int i = 3; i * i < n; i += 2) {
 
-            if (isPrime[i]) {
+            if (prime[i] == 1) {
 
-                for (int j = i * i; j < n; j += 2 * i) {
-                    isPrime[j] = false;
+                for (int j = i * i; j < n; j += i * 2) {
+                    prime[j] = 0;
                 }
             }
         }
 
+        // Count primes
         for (int i = 3; i < n; i += 2) {
-
-            if (isPrime[i]) {
+            if (prime[i] == 1) {
                 count++;
             }
         }
